78b7764c37df4dd7622b22554099f0dec6267061
SashaYKK <alexgan2009@mail.ru>
2026-04-09 17:20:27 +0300

    /////

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
+let tasks = [
+    {
+        title: "Спааааааать",
+        priority: "high-priority",
+        isCompleted: false,
+    },
+    {
+        title: "Полететь в космос",
+        priority: "",
+        isCompleted: false,
+    },
+    {
+        title: "Сварить пельмешки",
+        priority: "medium-priority",
+        isCompleted: false,
+    }
+]
+
 
 function start() {
-    console.log('Hi!')
-    alert('52')
+    console.log('Hi!');
+    alert('52');
 }
+
+document
+    .addEventListener('keydown',
+        (ev) => {
+            if (ev.key === 'Enter') {
+                addTask();
+            }
+        })
 
 const addButton = document.getElementById("add-btn");
 const tasklistEl = document.getElementById("task-list");
 const inputtasklistEl = document.getElementById("input-task");
 addButton.addEventListener('click', addTask);
-
-
-//addButton.onclick = start;
-
-
 
 function addTask() {
     const taskTitle = inputtasklistEl.value;
@@ -25,18 +46,33 @@ function addTask() {
             ev.current.classList.toggle('completed');
         });
         newTaskEl.innerHTML = `
-    <span class="task-content"> ${taskTitle} </span>
-    <div class="actions">
-        <button class="btn" onclick="this.parentNode.parentNode.remove()">
-            <span class="material-symbols-outlined">
-                delete
-            </span> </button> 
-    </div>
-    `
+        <span class="task-content"> ${taskTitle} </span>
+        <div class="actions">
+            <button class="btn" onclick="this.parentNode.parentNode.remove()">
+                <span class="material-symbols-outlined">
+                    delete
+                </span> </button> 
+        </div>`
 
         tasklistEl.append(newTaskEl)
-        inputtasklistEl.value=null
+        inputtasklistEl.value = null
     }
 }
 
+function createTask() {
+    const newTaskEl = document.createElement('li');
+        newTaskEl.classList.add("item");
+        newTaskEl.classList.add("low-priority");
+        newTaskEl.addEventListener('click', (ev) => {
+            ev.current.classList.toggle('completed');
+        });
+        newTaskEl.innerHTML = `
+        <span class="task-content"> ${taskTitle} </span>
+        <div class="actions">
+            <button class="btn" onclick="this.parentNode.parentNode.remove()">
+                <span class="material-symbols-outlined">
+                    delete
+                </span> </button> 
+        </div>`
 
+}
