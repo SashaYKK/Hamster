ff1fc4ef2c4c282ed096149cc8752f2ada1a4277
SashaYKK <alexgan2009@mail.ru>
2026-04-28 18:16:48 +0300

    ..............

--- index.js
+++ index.js
@@ -1,78 +1,88 @@
-let tasks = [
-    {
-        title: "Спааааааать",
-        priority: "high-priority",
-        isCompleted: false,
+const keyStorage = 'task-list';
+const taskStorage = {
+    save: (tasks) => {
+        const tasksAsStr = JSON.stringify(tasks);
+        localStorage.setItem(keyStorage, tasksAsStr);
     },
-    {
-        title: "Полететь в космос",
-        priority: "",
-        isCompleted: false,
-    },
-    {
-        title: "Сварить пельмешки",
-        priority: "medium-priority",
-        isCompleted: false,
+    get: () => {
+        const tasks = localStorage.getItem(keyStorage);
+        return JSON.parse(tasks);
+    },    
+    init: () => {
+        if (localStorage.getItem(keyStorage) === null) {
+            taskStorage.save([]);
+        }
     }
-]
+};
 
-
-function start() {
-    console.log('Hi!');
-    alert('52');
-}
-
-document
-    .addEventListener('keydown',
-        (ev) => {
-            if (ev.key === 'Enter') {
-                addTask();
-            }
-        })
-
+taskStorage.init();
+const taskListEl = document.getElementById("task-list");
 const addButton = document.getElementById("add-btn");
-const tasklistEl = document.getElementById("task-list");
-const inputtasklistEl = document.getElementById("input-task");
-addButton.addEventListener('click', addTask);
+const inputTaskEl = document.getElementById("input-task");
+addButton.onclick = addTask;
+
+let TASKS = taskStorage.get();
+TASKS.forEach(task => createTaskElement(task));
+
+document.addEventListener('keydown',
+    (ev) => {
+        if (ev.key === 'Enter') {
+            addTask();
+        }
+    })
 
 function addTask() {
-    const taskTitle = inputtasklistEl.value;
-
+    const taskTitle = inputTaskEl.value;
     if (taskTitle) {
-        const newTaskEl = document.createElement('li');
-        newTaskEl.classList.add("item");
-        newTaskEl.classList.add("low-priority");
-        newTaskEl.addEventListener('click', (ev) => {
-            ev.current.classList.toggle('completed');
-        });
-        newTaskEl.innerHTML = `
-        <span class="task-content"> ${taskTitle} </span>
-        <div class="actions">
-            <button class="btn" onclick="this.parentNode.parentNode.remove()">
-                <span class="material-symbols-outlined">
-                    delete
-                </span> </button> 
-        </div>`
 
-        tasklistEl.append(newTaskEl)
-        inputtasklistEl.value = null
+        const task = {
+            title: taskTitle,
+            isCompleted: false,
+            priority: 'low'
+        };
+
+        createTaskElement(task);
+        TASKS.push(task);
+        taskStorage.save(TASKS);
+
+        inputTaskEl.value = null;
     }
 }
 
-function createTask() {
+function createTaskElement(task) {
     const newTaskEl = document.createElement('li');
-        newTaskEl.classList.add("item");
-        newTaskEl.classList.add("low-priority");
-        newTaskEl.addEventListener('click', (ev) => {
-            ev.current.classList.toggle('completed');
-        });
-        newTaskEl.innerHTML = `
-        <span class="task-content"> ${taskTitle} </span>
-        <div class="actions">
-            <button class="btn" onclick="this.parentNode.parentNode.remove()">
-                <span class="material-symbols-outlined">
-                    delete
-                </span> </button> 
-        </div>`
+    newTaskEl.classList.add("item");
+    newTaskEl.classList.add(`${task.priority}-priority`);
 
+    if (task.isCompleted) {
+        newTaskEl.classList.add('completed');
+    }
+
+    newTaskEl.addEventListener('click', (ev) => {
+        ev.currentTarget.classList.toggle('completed');
+        task.isCompleted = !task.isCompleted;
+        taskStorage.save(TASKS);
+    });
+
+    
+
+    newTaskEl.innerHTML = `
+    <span class="task-content"> ${task.title} </span>
+    <div class="actions">
+        <button class="btn" >
+            <span class="material-symbols-outlined">
+                delete
+            </span> </button> 
+    </div>`
+
+    
+    newTaskEl
+            .querySelector('button.btn')
+            .addEventListener('click', (ev) => {
+            ev.currentTarget.parentNode.parentNode.remove();
+            TASKS = TASKS.filter(x => x.title != task.title);
+            taskStorage.save(TASKS)
+        });
+    
+    taskListEl.append(newTaskEl);
 }
